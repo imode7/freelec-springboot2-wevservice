1eb660271071b56ab305f8de3b9e282755f94322
imode7 <imode7@naver.com>
2020-05-20 09:36:12 +0900

    Hello Controller 테스트코드

---
+++ src/main/java/com/jojoIdu/book/springboot/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.jojoIdu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("Hello")
+    public String hello(){
+        return "Hello";
+    }
+}
